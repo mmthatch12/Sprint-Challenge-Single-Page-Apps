b275280cb5c8a8d4ecfe61e3308417e8cc7cd85b
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-26 10:36:50 -0600

    finished implementing CharacterCard.js into CharacterList.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default function CharacterCard ({ image, name, species, status, location, origin }) {
-  return (<span>todo: location</span>
+  return ( 
+    <div>
+      <img src={image} />
+      <h1>{name}</h1>
+      <h3>{species}</h3>
+      <h3>{status}</h3>
+      <h3>{location}</h3>
+      <h3>{origin}</h3>
+    </div>
+  )
 }
