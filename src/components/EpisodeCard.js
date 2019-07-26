d8d2c3073f31816dfb08ec497cc8439d45e53638
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-26 11:14:39 -0600

    worked on styling EpisodeCard

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const EpisodeDiv = styled.div`
+  border: 1px solid grey;
+  border-radius: 5px;
+  font-size: 1rem;
+  margin: 20px;
+`;
 
 export default function EpisodeCard ({ name, date, episodenum }) {
   // image={image}
   return (
-    <div>
+    <EpisodeDiv>
       <h1>{name}</h1>
       <h3>{date}</h3>
       <h3>{episodenum}</h3>
-    </div>
+    </EpisodeDiv>
   )
 }
