ce7db190d2b09b0884d0a46ec6ad926547bf97cf
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-26 09:34:16 -0600

    working on LocationsList.js

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
 export default function LocationsList() {
+    const[locationsList, setLocationsList] = useState([])
 
+    useEffect(() => {
+      Axios 
+        .get(`https://rickandmortyapi.com/api/locations/`)
+        .then(response => {
+          const theInfo = response.data.results
+          console.log('theInfo', response.data.results)
+          setLocationsList(theInfo)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+      // TODO: Add AJAX/API Request here - must run in `useEffect`
+      //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    }, [])
+
+    return (
+        <section className='character-list grid-view'>
+    
+          {locationsList.map(location => {
+              return (
+                <div> 
+                  <h2>{location.name}</h2>
+                  <h3>{location.type}</h3>
+                  <h3>{location.dimension}</h3>
+                </div> 
+              )}
+          )}
+          
+        </section>
+      )
 }
