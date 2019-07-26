b878af8d6aed256c49d370d4a758761c81fa5943
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-26 09:13:55 -0600

    working on axios for CharacterList

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+//https://rickandmortyapi.com/api/character/
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const[characterList, setCharacterList] = useState([])
 
   useEffect(() => {
+    axios 
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setMovie(response.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, [])
